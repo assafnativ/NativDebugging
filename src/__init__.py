bbdea945d6402668762218448fa6cb337adaa104
Pagefault <nativ.assaf@gmail.com>
2011-06-21 12:41:09 +0000

    Fixed installer for Unix installation.

--- src/__init__.py
+++ src/__init__.py
@@ -10,6 +10,9 @@ import sys
 if sys.platform.lower().startswith('win'):
     import Win32
     __all__.append('Win32')
-else:
+elif sys.platform.lower().startswith('linux'):
     import Linux
     __all__.append('Linux')
+else:
+    import Unix
+    __all__.append('Unix')
