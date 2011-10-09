245512d7b8ff3cdbd49a54f329a0630ce89c9ade
Pagefault <nativ.assaf@gmail.com>
2011-10-09 19:45:43 +0000

    License was missing

--- src/Win32/MemReaderBaseWin.py
+++ src/Win32/MemReaderBaseWin.py
@@ -1,3 +1,24 @@
+#
+#   MemoryReaderBaseWin.py
+#
+#   MemoryReader - Remote process memory inspection python module
+#   https://svn3.xp-dev.com/svn/nativDebugging/
+#   Nativ.Assaf+debugging@gmail.com
+#   Copyright (C) 2011  Assaf Nativ
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
 
 from ..MemReaderBase import *
 from .Win32Structs import *
