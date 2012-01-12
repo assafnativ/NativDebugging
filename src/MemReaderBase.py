5630988b5743beffc39524b3078b0cd6f61135a6
Pagefault <nativ.assaf@gmail.com>
2012-01-12 19:47:32 +0000

    Added alias for few reading functions. Added some documentation.

--- src/MemReaderBase.py
+++ src/MemReaderBase.py
@@ -13,7 +13,18 @@ class MemReaderBase( RecursiveFind ):
         self.findInSymbols  = None
         self.findSymbol     = None        
 
+        self.dd = self.readNPrintDwords
+        self.db = self.readNPrintBin
+        self.dw = self.readNPrintWords
+        self.dq = self.readNPrintQwords
+
     def resolveOffsetsList( self, start, l, isVerbos=False, isLookingForCycles=True ):
+        """
+        A quick way jump from one pointer to another.
+        Starting from start address, reading the pointer at (start + l[0]), and then reads the pointer at the last result + l[1] and so on.
+        The verbos option would print all of the pointers including the starting address.
+        The isLookingForCycles option would alert the user in case one pointer is read more than once during the offsets walk.
+        """
         result = [start]
         readFail = False
         cycleFound = False
@@ -52,24 +63,36 @@ class MemReaderBase( RecursiveFind ):
         return result
 
     def readNPrintQwords( self, addr, length=0x100, isNoBase=True, itemsInRow=4 ):
+        """
+        Display memory as QWords tabls, does not return anything
+        """
         if isNoBase:
             printAsQwordsTable(self.readMemory(addr, length), itemsInRow=itemsInRow)
         else:
             printAsQwordsTable(self.readMemory(addr, length), addr, itemsInRow=itemsInRow)
 
     def readNPrintDwords( self, addr, length=0x100, isNoBase=True, itemsInRow=8 ):
+        """
+        Display memory as DWords tabls, does not return anything
+        """
         if isNoBase:
             printAsDwordsTable(self.readMemory(addr, length), itemsInRow=itemsInRow)
         else:
             printAsDwordsTable(self.readMemory(addr, length), addr, itemsInRow=itemsInRow)
 
     def readNPrintWords( self, addr, length=0x100, isNoBase=True, itemsInRow=0x10 ):
+        """
+        Display memory as Words tabls, does not return anything
+        """
         if isNoBase:
             printAsWordsTable(self.readMemory(addr, length), itemsInRow=itemsInRow)
         else:
             printAsWordsTable(self.readMemory(addr, length), addr, itemsInRow=itemsInRow)
 
     def readNPrintBin( self, addr, length=0x100, isNoBase=True, itemsInRow=0x10 ):
+        """
+        Display memory as bytes tabls, does not return anything
+        """
         if isNoBase:
             print(DATA(self.readMemory(addr, length), itemsInRow=itemsInRow))
         else:
