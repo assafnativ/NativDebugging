9aa4abe80747ef00c215ef3700f498d94bc57364
Pagefault <nativ.assaf@gmail.com>
2012-12-31 13:51:49 +0000

    Updated the file MemoryReader

--- src/File/FileReader.py
+++ src/File/FileReader.py
@@ -31,26 +31,29 @@ except ImportError as e:
     IS_DISASSEMBLER_FOUND = False
 from struct import pack, unpack
 
-def attach(targetFileName, pointer_size=4, endianity='='):
-    return MemoryReader(targetFileName, pointer_size, endianity)
+def attach(targetFileName, file_start_offset=0, loading_offset=0, pointer_size=4, endianity='='):
+    return MemoryReader(targetFileName, file_start_offset, loading_offset, pointer_size, endianity)
 
 class MemoryReader( MemReaderBase, MemWriterInterface, GUIDisplayBase ):
-    def __init__( self, targetFileName, pointer_size=4, endianity='='):
+    def __init__( self, targetFileName, file_start_offset=0, loading_offset=0, pointer_size=4, endianity='='):
         self._file = file(targetFileName, 'rb+')
         MemReaderBase.__init__(self)
         self._POINTER_SIZE = pointer_size
         self._DEFAULT_DATA_SIZE = 4
         self._ENDIANITY = endianity
+        self._START = file_start_offset
+        self._LOADING_ADDR = loading_offset
+        self._ADDR_DELTA = file_start_offset - loading_offset
         # Find end of file
         self._file.seek(0, 2)
         self._file_size = self._file.tell()
-        self._file.seek(0, 1)
+        self._file.seek(self._ADDR_DELTA, 1)
 
     def __del__( self ):
         self._file.close()
 
     def readAddr( self, addr ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if 4 == self._POINTER_SIZE:
             return unpack(self._ENDIANITY + 'L', self._file.read(4))
         elif 8 == self._POINTER_SIZE:
@@ -59,28 +62,29 @@ class MemoryReader( MemReaderBase, MemWriterInterface, GUIDisplayBase ):
             raise Exception("Unknown pointer size")
 
     def readQword( self, addr ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         return unpack(self._ENDIANITY + 'Q', self._file.read(8))[0]
 
     def readDword( self, addr ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         return unpack(self._ENDIANITY + 'L', self._file.read(4))[0]
 
     def readWord( self, addr ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         return unpack(self._ENDIANITY + 'H', self._file.read(2))[0]
 
     def readByte( self, addr ):
+        self._file.seek(addr + self._ADDR_DELTA)
         return ord(self.readMemory(addr, 1))
 
     def readMemory( self, addr, length ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         return self._file.read(length)
 
     def readString( self, addr, maxSize=None, isUnicode=False ):
         result = ''
         bytesCounter = 0
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
 
         while True:
             if False == isUnicode:
@@ -103,7 +107,7 @@ class MemoryReader( MemReaderBase, MemWriterInterface, GUIDisplayBase ):
                 return result
 
     def writeAddr( self, addr, data ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if isinstance(data, (int, long)):
             if 4 == self._POINTER_SIZE:
                 data = pack(self._ENDIANITY + 'L', data)
@@ -114,33 +118,35 @@ class MemoryReader( MemReaderBase, MemWriterInterface, GUIDisplayBase ):
         self._file.write(data)
 
     def writeQword( self, addr, data ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if isinstance(data, (int, long)):
             data = pack('<Q', data)
         self._file.write(data)
 
     def writeDword( self, addr, data ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if isinstance(data, (int, long)):
             data = pack('<L', data)
         self._file.write(data)
 
     def writeWord( self, addr, data ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if isinstance(data, (int, long)):
             data = pack('<H', data)
         self._file.write(data)
 
     def writeByte( self, addr, data ):
-        self._file.seek(addr)
+        self._file.seek(addr + self._ADDR_DELTA)
         if isinstance(data, (int, long)):
             data = chr(data)
         self._file.write(data)
 
     def writeMemory( self, addr, data ):
-        return 
+        self._file.self(addr + self._ADDR_DELTA)
+        self._file.write(data)
 
     def isAddressValid( self, addr ):
+        addr += self._ADDR_DELTA
         if addr > 0 and addr < self._file_size:
             return True
         return False
@@ -164,4 +170,9 @@ class MemoryReader( MemReaderBase, MemWriterInterface, GUIDisplayBase ):
     def getEndianity(self):
         return self._ENDIANITY
 
+    def getLoadingAddress(self):
+        return self._LOADING_ADDR
+
+    def getFileOffset(self):
+        return self._START
 
