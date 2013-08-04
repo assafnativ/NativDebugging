e96a47cb41038fb904a22688ab3ffb3efeb1d71e
Pagefault <nativ.assaf@gmail.com>
2013-08-04 13:51:29 +0000

    Fixed bug

--- src/Linux/SharedMemReader.py
+++ src/Linux/SharedMemReader.py
@@ -22,7 +22,7 @@
 
 import sys
 import struct
-from ctypes import c_void_p, c_uint8, c_uint16, c_uint32, c_uint64, cdll
+from ctypes import c_void_p, c_uint8, c_uint16, c_uint32, c_uint64, cdll, sizeof
 import subprocess
 from subprocess import Popen
 
