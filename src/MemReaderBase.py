3a5ae77eceda2fad077a77dbee22f3ff15ec7785
Pagefault <nativ.assaf@gmail.com>
2013-09-02 21:09:17 +0000

    License added

--- src/MemReaderBase.py
+++ src/MemReaderBase.py
@@ -1,3 +1,27 @@
+#
+#   MemReaderBase.py
+#
+#   MemReaderBase - Implements the common methods of all memory readers
+#   https://svn3.xp-dev.com/svn/nativDebugging/
+#   Nativ.Assaf+debugging@gmail.com
+#   Copyright (C) 2011  Assaf Nativ
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+# Platform independent
+
 
 from abc import ABCMeta
 from .Interfaces import MemReaderInterface, ReadError
--- src/RecursiveFind.py
+++ src/RecursiveFind.py
@@ -1,3 +1,28 @@
+#
+#   RecursiveFind.py
+#
+#   RecursiveFind - Implements a memory search technic called recursive
+#   search.
+#   https://svn3.xp-dev.com/svn/nativDebugging/
+#   Nativ.Assaf+debugging@gmail.com
+#   Copyright (C) 2011  Assaf Nativ
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+# Platform independent
+
 
 from abc import ABCMeta
 from .Interfaces import MemReaderInterface, ReadError
