edc03787f29d28d8871303b244a4949ece62829a
Assaf Nativ <nativ.assaf@gmail.com>
2017-07-13 16:51:21 +0300

    Bump version 30

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ packagesDirs['NativDebugging/Patterns'] = 'src/Patterns'
 from distutils.core import setup
 setup(
         name = 'NativDebugging',
-        version = '29',
+        version = '30',
         description = 'Debugging tools for many platforms',
         author = 'Assaf Nativ',
         author_email = 'Nativ.Assaf@gmail.com',
